239ac97f2aed6f2d47bd5a467ec4dfc26e145d56
Abhishek Kumar Roy <thunderoy013@gmail.com>
2016-02-29 09:28:14 +0530

    created setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Nasdaq project"""
+from setuptools import find_packages, setup
+
+setup(name = 'Nasdaq-Value',
+    version = '0.1',
+    description = "Nasdaq module.",
+    long_description = "A module to get nasdaq value of a company.",
+    platforms = ["Linux"],
+    author = "Abhishek Kumar Roy",
+    author_email = "thuderoy013@gmail.com",
+    url = "thunderoy.wordpress.com",
+    license = "MIT",
+    packages = find_packages()
+    )
